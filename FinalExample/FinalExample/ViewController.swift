f748213d321a0fc596fb1891806e938fbc25cdb6
JANGHUIJO <jhjo.tech@gmail.com>
2018-07-30 11:25:39 +0900

    practice init commit

---
+++ FinalExample/FinalExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalExample
+//
+//  Created by Jo JANGHUI on 2018. 7. 30..
+//  Copyright © 2018년 JhDAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
